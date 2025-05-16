8a3729a324fc455825c1862ce1796b7c81873247
Andrés Camilo Ocampo Castillo <135870143+voicimoncode@users.noreply.github.com>
2025-05-15 19:31:45 -0500

    Update SuperHeroViewHolder.kt

--- app/src/main/java/com/example/recyclerviewexamplo/adapter/SuperHeroViewHolder.kt
+++ app/src/main/java/com/example/recyclerviewexamplo/adapter/SuperHeroViewHolder.kt
@@ -1,27 +1,16 @@
 package com.example.recyclerviewexamplo.adapter
 
 import android.view.View
-import android.widget.Toast
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.example.recyclerviewexamplo.SuperHero
-import com.example.recyclerviewexamplo.databinding.ItemSuperheroBinding
+import com.example.recyclerviewexamplo.Element
+import com.example.recyclerviewexamplo.R
 
-class SuperHeroViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+class ElementViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
-    val binding = ItemSuperheroBinding.bind(view)
+    private val tvName: TextView = view.findViewById(R.id.tvName)
 
-    fun render(
-        superHeroModel: SuperHero,
-        onClickListener: (SuperHero) -> Unit,
-        onClickDelete: (Int) -> Unit
-    ) {
-        binding.tvSuperHeroName.text = superHeroModel.superhero
-        binding.tvRealName.text = superHeroModel.realName
-        binding.tvPublisher.text = superHeroModel.publisher
-        Glide.with(binding.ivSuperHero.context).load(superHeroModel.photo).into(binding.ivSuperHero)
-        binding.ivSuperHero.setOnClickListener { onClickListener(superHeroModel) }
-        itemView.setOnClickListener { Toast.makeText(itemView.context, "KotlinMan", Toast.LENGTH_SHORT).show()}
-        binding.btnDelete.setOnClickListener {  onClickDelete(adapterPosition) }
+    fun render(element: Element) {
+        tvName.text = element.name
     }
 }
