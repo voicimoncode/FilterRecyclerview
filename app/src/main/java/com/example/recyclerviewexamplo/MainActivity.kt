a7715356793c3c3452697d5e3c4609307eeb578d
Andrés Camilo Ocampo Castillo <135870143+voicimoncode@users.noreply.github.com>
2025-05-15 22:15:12 -0500

    Update MainActivity.kt

--- app/src/main/java/com/example/recyclerviewexamplo/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewexamplo/MainActivity.kt
@@ -1,27 +1,6 @@
-package com.example.recyclerviewexamplo
-
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.recyclerviewexamplo.adapter.ElementAdapter
-import com.example.recyclerviewexamplo.databinding.ActivityMainBinding
-
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-    private val elementList: List<Element> = ElementProvider.elementList
-    private lateinit var adapter: ElementAdapter
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        initRecyclerView()
+lifecycleScope.launch {
+    val data = withContext(Dispatchers.IO) {
+        getDataFromNetwork()
     }
-
-    private fun initRecyclerView() {
-        adapter = ElementAdapter(elementList)
-        binding.recyclerElement.layoutManager = LinearLayoutManager(this)
-        binding.recyclerElement.adapter = adapter
-    }
+    updateUI(data)
 }
