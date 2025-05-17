e63e94d0748aa93d7843c488b996469a219eb12e
Andrés Camilo Ocampo Castillo <135870143+voicimoncode@users.noreply.github.com>
2025-05-16 20:36:25 -0500

    Update MainActivity.kt

--- app/src/main/java/com/example/recyclerviewexamplo/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewexamplo/MainActivity.kt
@@ -1,6 +1,12 @@
-lifecycleScope.launch {
-    val data = withContext(Dispatchers.IO) {
-        getDataFromNetwork()
+class SimpleVolley(val context: Context) {
+
+    fun getData(url: String, callback: (JSONObject?) -> Unit) {
+        val queue = Volley.newRequestQueue(context)
+        val request = JsonObjectRequest(Request.Method.GET, url, null,
+            { response -> callback(response) },
+            { error -> callback(null) }
+        )
+        queue.add(request)
     }
-    updateUI(data)
 }
+
